1cac697b8330702ddac719959a1b1907b4436d3c
StageGuard <1355416608@qq.com>
2020-07-21 09:24:12 +0800

    Move version control to github

--- source/wrapper.js
+++ source/wrapper.js
@@ -2,6 +2,8 @@
 "use strict";
 var emitter = events.emitter(threads.currentThread());
 threads.start(function() {
-	emitter.emit("evaluate", http.get("https://cdn.jsdelivr.net/gh/StageGuard/SkyAutoPlayerScript@" + http.get("https://gitee.com/stageguard/SkyAutoPlayerScript/raw/master/gitVersion").body.string() + "/source/SkyAutoplayer.js").body.string());
+  emitter.emit("evaluate", http.get("https://cdn.jsdelivr.net/gh/StageGuard/SkyAutoPlayerScript@" + http.get("https://cdn.jsdelivr.net/gh/StageGuard/SkyAutoPlayerScript@master/gitVersion").body.string() + "/source/SkyAutoplayer.js").body.string());
 });
-emitter.on('evaluate', function(s){eval(s);});
+emitter.on('evaluate', function(s){
+  eval(s);
+});;
